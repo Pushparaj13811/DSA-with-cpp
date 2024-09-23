a5bef89c5e163a23c926998b69b5b4db20104771
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-09-23 10:41:35 +0530

    Add code for calculating the sum of digits in a number

---
+++ functions/sumOfDigits.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int sumOfDigits(int n){
+    int sum = 0;
+    while (n > 0)
+    {
+        int lastDigit = n % 10;
+        sum += lastDigit;
+        n = n / 10;
+    }
+    
+    return sum; 
+
+}
+int main(){
+
+    cout << sumOfDigits(123) << endl;
+    
+    return 0;
+}
