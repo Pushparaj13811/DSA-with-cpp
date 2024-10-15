d352f740f30041441078cf94692794e609f826bb
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-10-15 17:43:03 +0530

    Optimize product of array except self calculation

--- vectors/prodOfArrExcSelf.cpp
+++ vectors/prodOfArrExcSelf.cpp
@@ -51,11 +51,25 @@ vector<int> prefix(vector<int> nums){
 
 // Fully optimized code 
 
+vector<int> optimisedResult(vector<int> nums){
+    int len = nums.size();
+    vector<int> result(len, 1); 
+    for(int i = 1; i < len; i++){
+        result[i] = result[i-1] * nums[i-1];
+    }
+    cout << "Prefix : ";
+    print(result);
+    cout << endl;
 
+    for(int i = len - 2; i >= 0; i--){
+        result[i + 1] = result[i + 1] * nums[i + 1];
+    }
+    return result;
+}
 
 int main(){
     vector<int> nums = {1,2,3,4};
-    print(prefix(nums));
+    print(optimisedResult(nums));
 
     return 0;
 }
